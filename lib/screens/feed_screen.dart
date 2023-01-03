1d725ed68376bdbc27f9e2e2637035370811ec1b
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-03 20:01:44 +0530

    add AppBar

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,10 +1,31 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/logo.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.messenger_outline,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

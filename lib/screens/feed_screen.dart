daf70ca0a5d371f3ae004c6b9a3e4be6e0267f7d
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-03 20:05:47 +0530

    add post card class to feed_screen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/post_card.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
@@ -26,6 +27,7 @@ class FeedScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: const PostCard(),
     );
   }
 }

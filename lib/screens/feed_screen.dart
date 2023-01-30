89f464ca5d4e1ee1c288350ac183993d2b06f96d
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-30 19:40:30 +0530

    add streambuilder and listview builder

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -27,7 +28,24 @@ class FeedScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const PostCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => Container(
+            child: PostCard(
+              snap: snapshot.data!.docs[index].data(),
+            ),
+          ),);
+        },
+      ),
     );
   }
 }

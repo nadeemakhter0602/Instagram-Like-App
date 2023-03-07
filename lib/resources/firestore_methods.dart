b87f9d17e02035af4df7a659725f99038f12ff22
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-03-07 19:42:17 +0530

    add firestore methods class for uploading posts

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:instagram_flutter/models/posts.dart';
+import 'package:instagram_flutter/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // uploading posts
+  Future<String> uploadPost(
+    String description,
+    Uint8List file,
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    String res = "Some error occurred";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+
+      String postId = const Uuid().v1();
+
+      Post post = Post(
+        description: description,
+        uid: uid,
+        username: username,
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+        likes: [],
+      );
+
+      _firestore.collection('posts').doc(postId).set(
+            post.toJson(),
+          );
+      res = 'Success';
+    } catch (err) {
+      res = err.toString();
+    }
+
+    return res;
+  }
+}
